0efd51639b489290156a61919bf8fec8cb20f2e0
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2024-09-28 15:35:03 +0200

    Advancements

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     let timerDisplay = document.getElementById('timer');
     let titleDisplay = document.getElementById('title');
     let startButton = document.getElementById('startButton');
     let clearButton = document.getElementById('clearButton');
-    let display = document.getElementById('display');
+    let showButton = document.getElementById('showButton');
+    let configuration = document.getElementById('configuration');
+    let controls = document.getElementById('controls');
+    
     let countdown; 
     let totalSecondes; // Temps restant pour le minuteur principal
-    let isPaused = true; // Marche ou pause
-    
+    let isPaused = true; //marche ou pause
+    let isWorkPhase = true; // Savoir si c'est la phase de travail ou de pause
 
     // Fonction pour un affichage clair
     function formatTime(minutes, secondes) {
         return String(minutes).padStart(2, '0') + ":" + String(secondes).padStart(2, '0');
     }
 
-    function formatTime(minutesPause, secondesPause) {
-        return String(minutesPause).padStart(2, '0') + ":" + String(secondesPause).padStart(2, '0');
-    }
-
     // Fonction pour démarrer le timer de pause
     function timerPause() {
         titleDisplay.textContent = "Pause";
-        titleDisplay.classList.add('highlight');
-        setTimeout(function() {
-            titleDisplay.classList.remove('highlight');
-        }, 2000); // 2000 ms 
-
+        let timer = document.getElementById('timer');
+        timer.style.animation = "borderAnimationEnd 2s ease-in-out forwards";
+        
+        
         let minutesPauseInput = parseInt(document.getElementById('minutesPause').value) || 0;
         let secondesPauseInput = parseInt(document.getElementById('secondesPause').value) || 0;
         
         totalSecondes = (minutesPauseInput * 60) + secondesPauseInput;
         if (totalSecondes == 0) {
-            totalSecondes = (5 * 60); // 25min par défaut
+            totalSecondes = (5 * 60); // 5 minutes par défaut pour la pause
         }
         timerDisplay.textContent = formatTime(Math.floor(totalSecondes / 60), totalSecondes % 60);
 
@@ -44,7 +41,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 startButton.textContent = "Démarrer"; 
                 let timer = document.getElementById('timer');
                 timer.style.animation = "none";
-                totalSecondes = null; // reinitialiser le temps restant
+                totalSecondes = null; // réinitialiser le temps restant
+                isWorkPhase = true; // revenir à la phase de travail
+                startTimer(); // Redémarrer le timer de travail après la pause
             } else {
                 totalSecondes--;
 
@@ -56,63 +55,76 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }, 1000);
     }
-    // gestion du bouton demarrer
 
-    startButton.addEventListener('click', function() {
+    // Fonction pour démarrer le timer de travail
+    function startWorkTimer() {
         titleDisplay.textContent = "Phase de travail";
-        
         let timer = document.getElementById('timer');
-        timer.style.animation = "float 2s ease-in-out infinite";
+        timer.style.animation = "borderAnimationStart 2s ease-in-out forwards";
+        let minutesInput = parseInt(document.getElementById('minutes').value) || 0;
+        let secondsInput = parseInt(document.getElementById('secondes').value) || 0;
 
-        setTimeout(function() {
-            titleDisplay.classList.remove('highlight');
-        }, 2000); // 2000 ms 
-        if (isPaused) {
-            // Récupérer les valeurs
-            let minutesInput = parseInt(document.getElementById('minutes').value) || 0;
-            let secondsInput = parseInt(document.getElementById('secondes').value) || 0;
-
-            // Pour le premier démarrage
-            if (!totalSecondes) {
-                titleDisplay.classList.add('highlight');
-                totalSecondes = (minutesInput * 60) + secondsInput;
-                if (totalSecondes == 0) {
-                    totalSecondes = (25 * 60); // 25min par défaut
-                }
+        // Pour le premier démarrage
+        if (!totalSecondes) {
+            
+            totalSecondes = (minutesInput * 60) + secondsInput;
+            if (totalSecondes == 0) {
+                totalSecondes = (25 * 60); // 25 minutes par défaut pour le travail
             }
+        }
 
-            // afficher la durée
-            timerDisplay.textContent = formatTime(Math.floor(totalSecondes / 60), totalSecondes % 60);
+        // afficher la durée
+        timerDisplay.textContent = formatTime(Math.floor(totalSecondes / 60), totalSecondes % 60);
 
-            //demarrer ou stopper le minuteur principal
-            countdown = setInterval(function() {
-                if (totalSecondes <= 0) {
-                    clearInterval(countdown);
-                    timerPause(); //Lancer le timer de pause de 5 minutes après la fin du premier
-                    
-                } else {
-                    totalSecondes--;
+        // démarrer le minuteur de travail
+        countdown = setInterval(function() {
+            if (totalSecondes <= 0) {
+                clearInterval(countdown);
+                timerPause(); // Lancer le timer de pause après la fin du travail
+                isWorkPhase = false; // Marquer que la phase de travail est terminée
+            } else {
+                totalSecondes--;
 
-                    let minutesRestantes = Math.floor(totalSecondes / 60);
-                    let secondesRestantes = totalSecondes % 60;
+                let minutesRestantes = Math.floor(totalSecondes / 60);
+                let secondesRestantes = totalSecondes % 60;
 
-                    // afficher le nouveau temps restant
-                    timerDisplay.textContent = formatTime(minutesRestantes, secondesRestantes);
-                }
-            }, 1000);
+                // afficher le nouveau temps restant
+                timerDisplay.textContent = formatTime(minutesRestantes, secondesRestantes);
+            }
+        }, 1000);
+    }
+
+    //demarrage du timer de travail ou de pause en fonction de la situation
+    function startTimer() {
+        if (isWorkPhase) {
+            startWorkTimer(); // Démarrer la phase de travail
+        } else {
+            timerPause(); // Démarrer la pause
+        }
+    }
+
+    // Gestion du bouton démarrer/suspendre
+    startButton.addEventListener('click', function() {
+        if (isPaused) {
+            let timer = document.getElementById('timer');
+            timer.style.animation = "borderAnimationStart 2s ease-in-out forwards";
+            startTimer();
             startButton.textContent = "Suspendre"; 
             isPaused = false; 
-
         } else {
-            
+            let timer = document.getElementById('timer');
+            timer.style.animation = "borderAnimationEnd 2s ease-in-out forwards";
             clearInterval(countdown); 
             startButton.textContent = "Reprendre"; 
             isPaused = true; 
+            
         }
     });
 
-    // bouton reinitialiser
+    // Gestion du bouton réinitialiser
     clearButton.addEventListener('click', function() {
+        let timer = document.getElementById('timer');
+        timer.style.animation = "borderAnimationEnd 2s ease-in-out forwards";
         titleDisplay.textContent = "Pomodoro";
         clearInterval(countdown); 
         totalSecondes = null;
@@ -121,53 +133,32 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('minutes').value = ''; 
         document.getElementById('secondes').value = ''; 
         isPaused = true; 
+        isWorkPhase = true; // Réinitialiser à la phase de travail
     });
-    
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    let showButton = document.getElementById('showButton');
-    let configuration = document.getElementById('configuration');
-
+    // Gestion du bouton showButton pour afficher/masquer les sections
     showButton.addEventListener('click', function() {
+        // Configuration du minuteur
         if (configuration.style.display === "none") {
             configuration.style.display = "flex"; 
         } else {
             configuration.style.display = "none";
         }
-    });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    let showButton = document.getElementById('showButton');
-    let title = document.getElementById('title');
-
-    showButton.addEventListener('click', function() {
-        if (title.style.display === "none") {
-            title.style.display = "flex"; 
+        // Titre
+        if (titleDisplay.style.display === "none") {
+            titleDisplay.style.display = "flex"; 
         } else {
-            title.style.display = "none";
+            titleDisplay.style.display = "none";
         }
-    });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    let showButton = document.getElementById('showButton');
-    let timer = document.getElementById('timer');
-    let controls = document.getElementById('controls');
-
-    showButton.addEventListener('click', function() {
-        if (timer.style.display === "none") {
-            timer.style.display = "flex";
+        // Timer et contrôles
+        if (timerDisplay.style.display === "none") {
+            timerDisplay.style.display = "flex";
             controls.style.display = "flex";
-
         } else {
-            timer.style.display = "none";
+            timerDisplay.style.display = "none";
             controls.style.display = "none";
-
         }
     });
 });
-
-
