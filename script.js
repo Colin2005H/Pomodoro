7270e1b81b8128658d227f2f123c63b9da943a4c
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2024-09-30 21:13:39 +0200

    Add restrictions for the time

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let configuration = document.getElementById('configuration');
     let controls = document.getElementById('controls');
     var startSound = new Audio("Sounds/StartSound.mp3");
+    const chime = new Audio("Sounds/chime.mp3");
     
     let countdown;
     let totalSecondes = null;
@@ -30,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function timerPause() {
         let minutesPauseInput = parseInt(document.getElementById('minutesPause').value) || 0;
         let secondesPauseInput = parseInt(document.getElementById('secondesPause').value) || 0;
-        
+        chime.play();
         totalSecondes = (minutesPauseInput * 60) + secondesPauseInput;
         if (totalSecondes == 0) {
             totalSecondes = 5 * 60;
@@ -62,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (totalSecondes === null) {
             totalSecondes = (minutesInput * 60) + secondsInput;
+            
             if (totalSecondes == 0) {
                 totalSecondes = 25 * 60;
             }
@@ -119,18 +121,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // affichage du menu de configuration 
     showButton.addEventListener('click', function() {
-        configuration.style.display = (configuration.style.display === "none") ? "flex" : "none";
-        titleDisplay.style.display = (titleDisplay.style.display === "none") ? "flex" : "none";
-        timerDisplay.style.display = (timerDisplay.style.display === "none") ? "flex" : "none";
-        controls.style.display = (controls.style.display === "none") ? "flex" : "none";
 
         let minutesInput = parseInt(document.getElementById('minutes').value) || 0;
         let secondsInput = parseInt(document.getElementById('secondes').value) || 0;
+        let minutesPauseInput = parseInt(document.getElementById('minutesPause').value) || 0;
+        let secondesPauseInput = parseInt(document.getElementById('secondesPause').value) || 0;
+        
+        let totalSecondesPause = (minutesPauseInput * 60) + secondesPauseInput;
         totalSecondes = (minutesInput * 60) + secondsInput;
-        timerDisplay.textContent = formatTime(Math.floor(totalSecondes / 60), totalSecondes % 60);
-        startButton.textContent = "Démarrer"; 
-        clearInterval(countdown); 
-        totalSecondes = null;
-        isWorkPhase = true;
+        if(totalSecondes > 7200){
+            alert("Entrez un temps de travail inférieur à 2H");
+        }
+
+        else if(totalSecondesPause > 3600){
+            alert("Entrez un temps de pause inférieur à 1H");
+        }
+
+
+        else{
+            configuration.style.display = (configuration.style.display === "none") ? "flex" : "none";
+            titleDisplay.style.display = (titleDisplay.style.display === "none") ? "flex" : "none";
+            timerDisplay.style.display = (timerDisplay.style.display === "none") ? "flex" : "none";
+            controls.style.display = (controls.style.display === "none") ? "flex" : "none";
+            timerDisplay.textContent = formatTime(Math.floor(totalSecondes / 60), totalSecondes % 60);
+            startButton.textContent = "Démarrer"; 
+            clearInterval(countdown); 
+            totalSecondes = null;
+            isWorkPhase = true;
+        }
+        
     });
 });
